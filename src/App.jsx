0897c6ff97d0e7a7cd5ed295664e021ba3c79e0a
Danny Chen <dannychen@Dannys-Air.lan1>
2025-01-04 22:20:52 -0500

    just finished delete func; now on toastify sec

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,50 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      {/* Encasing pages in a parent layout file allows it to be applied to pages you want */}
-
-      <Route index element={ <HomePage /> } />
-      <Route path='/jobs' element={ <JobsPage /> } />
-      <Route path='/add-job' element={ <AddJobPage /> } />
-      <Route path='/jobs/:id' element={ <JobPage /> } loader={ jobLoader } />
-
-      {/* Asterisk sign is a "catch all"; so same error page pops whenever there's error */}
-      <Route path='*' element={ <NotFoundPage /> } />
-    </Route>
-  )
-);
-
 const App = () => {
+  // NOTE: All request will go here
+  // Video mentioned how you can use Context API and redox for bigger applications
+    // For small app, putting all request here is fine
+
+  // Add new job
+  const addJob = async (newJob) => {
+    // console.log(newJob);
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+
+    return;
+  }
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${ id }`, {
+      method: 'DELETE'
+    });
+
+    return;
+  };
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        {/* Encasing pages in a parent layout file allows it to be applied to pages you want */}
+  
+        <Route index element={ <HomePage /> } />
+        <Route path='/jobs' element={ <JobsPage /> } />
+        <Route path='/add-job' element={ <AddJobPage addJobSubmit={ addJob } /> } />
+        <Route path='/jobs/:id' element={ <JobPage deleteJob={ deleteJob }/> } loader={ jobLoader } />
+  
+        {/* Asterisk sign is a "catch all"; so same error page pops whenever there's error */}
+        <Route path='*' element={ <NotFoundPage /> } />
+      </Route>
+    )
+  );
+
   return (
     <RouterProvider router={ router }/>
   )
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 // Needed to allow layout to apply to pages you desired
 import { Outlet } from 'react-router-dom';
-
 import Navbar from '../components/Navbar';
-
-
 
 const MainLayout = () => {
   return (
     <>
         {/* Components you want on many pages (e.g. <Navbar />) */}
         <Navbar />
-
         <Outlet />
     </>
   )
